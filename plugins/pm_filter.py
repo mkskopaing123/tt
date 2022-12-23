c18b4ab11d7407b65fcbdcd31b465c6668487025
Hansaka Anuhas <120485409+anuhas1233@users.noreply.github.com>
2022-12-23 12:28:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -756,8 +756,8 @@ async def auto_filter(client, msg, spoll=False):
             await k.delete() if settings['auto_delete'] else None
     else:
         k = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-    if spoll:
-        await msg.message.delete()
+        if spoll:
+                await msg.message.delete()
 
 
 async def advantage_spell_chok(msg):
